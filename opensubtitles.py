fb9b0f9fac45a4252cf67ed83232da08dd032e45
ShadowTemplate <taneburgo@gmail.com>
2015-11-29 22:35:32 +0100

    * Added methods to OS wrapper * Added utils

--- opensubtitles.py
+++ opensubtitles.py
@@ -1,45 +1,47 @@
 from xmlrpc.client import ServerProxy
 
 
-def _get_from_data_on_success_or_none(data, key):
-    status = data.get('status').split()[0]
-    return data.get(key) if '200' == status else None
-
-
 class OpenSubtitles(object):
     ENDPOINT = 'http://api.opensubtitles.org/xml-rpc'
+    DEFAULT_LANGUAGE = 'eng'
+    DEFAULT_USER_AGENT = 'OSTestUserAgent'
 
     def __init__(self):
         self.xmlrpc = ServerProxy(OpenSubtitles.ENDPOINT, allow_none=True)
         self.token = None
 
-    def login(self, user_agent):
-        assert user_agent is not None
-        data = self.xmlrpc.LogIn(user_agent)
+    def login(self, username='', password_md5='', lang=DEFAULT_LANGUAGE, user_agent=DEFAULT_USER_AGENT):
+        assert (username and password_md5) or (user_agent and user_agent is not OpenSubtitles.DEFAULT_USER_AGENT)
+        data = self.xmlrpc.LogIn(username, password_md5, lang, user_agent)
         self.token = _get_from_data_on_success_or_none(data, 'token')
+        return self.token
+
+    def set_token(self, token):
+        assert token
+        self.token = token
         return self.token
 
     def logout(self):
         data = self.xmlrpc.LogOut(self.token)
         return '200' in data.get('status')
 
-    def search_subtitles(self, params):
-        data = self.xmlrpc.SearchSubtitles(self.token, params)
-        return _get_from_data_on_success_or_none(data, 'data')
-
     def no_operation(self):
-        '''Return True if the session is actived, False othercase.
-
-        .. note:: this method should be called 15 minutes after last request to
-                  the xmlrpc server.
-        '''
         data = self.xmlrpc.NoOperation(self.token)
         return '200' in data.get('status')
 
-    def download_subtitles(self):
-        # array DownloadSubtitles( $token, array($IDSubtitleFile, $IDSubtitleFile,...) )
-        raise NotImplementedError
+    def search_subtitles_for_film(self, imdb_id, language):
+        data = self.xmlrpc.SearchSubtitles(self.token, [{'imdbid': imdb_id, 'sublanguageid': language}])
+        return _get_from_data_on_success_or_none(data, 'data')
 
-    def get_imdb_movie_details(self):
-        # array GetIMDBMovieDetails( $token, $imdbid )
-        raise NotImplementedError
+    def download_subtitles(self, subs_ids):
+        data = self.xmlrpc.DownloadSubtitles(self.token, subs_ids)
+        return _get_from_data_on_success_or_none(data, 'data')
+
+    def get_imdb_movie_details(self, imdb_id):
+        data = self.xmlrpc.GetIMDBMovieDetails(self.token, imdb_id)
+        return _get_from_data_on_success_or_none(data, 'data')
+
+
+def _get_from_data_on_success_or_none(data, key):
+    status = data.get('status').split()[0]
+    return data.get(key) if '200' == status else None
---
+++ test.py
@@ -0,0 +1,37 @@
+from opensubtitles import OpenSubtitles
+from utils import *
+
+os_client = OpenSubtitles()
+user_agent = 'ScriBa v1.0'
+imdb_id = '172495'
+token = '3eafo1ijeob0bh8cou2f8s36p1'
+# token = os_client.login(user_agent=user_agent)
+os_client.set_token(token)
+print('Token: {0}\n'.format(token))
+
+# get IMBDb details
+data = os_client.get_imdb_movie_details(imdb_id)
+print('Movie details: {0}\n'.format(data))
+
+# search for subtitles
+data = os_client.search_subtitles_for_film(imdb_id, 'eng')
+# note that each JSON contains also the direct download link
+print('Found {0} subtitle(s):\n{1}\n[...]\n'.format(len(data), pretty_format(data[0])))
+
+# extract all links/ids from data
+subs_links = [item['SubDownloadLink'] for item in data]
+print('Links: {0}\n'.format(subs_links))
+subs_ids = [item['IDSubtitle'] for item in data]
+print('Ids: {0}\n'.format(subs_ids))
+
+# retrieve encoded gzipped data from subtitle(s) id(s)
+data = os_client.download_subtitles(subs_ids[:5])  # get data only for the first 5 subs
+print('Sub data: {0}'.format(pretty_format(data)))
+
+# download subtitle file from encoded gzipped data
+# serialize_sub(data[0]['data'], 'decoded_sub.srt')
+
+# download subtitle file from url
+# download_file_from_url(subs_links[0], 'direct_downloaded.gz')
+
+# os_client.logout()
