3f97cc238710f368b626753e55ad2b08b916d0dd
ShadowTemplate <taneburgo@gmail.com>
2015-12-07 19:35:49 +0100

    * Improved utils

--- utils.py
+++ utils.py
@@ -1,56 +1,57 @@
 import hashlib
+from itertools import dropwhile
 import zlib
 import base64
-import urllib.request as urllib2
+import urllib.request
+
+from opensubtitles import OpenSubtitles
 
 
-def download_sub_from_encoded_data(encoded_data, output_file_path):
-    decoded_data = base64.b64decode(encoded_data)
-    decompressed_data = zlib.decompress(decoded_data, 16 + zlib.MAX_WBITS)
-    with open(output_file_path, "wb") as f:
-        f.write(decompressed_data)
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class ScriBa(metaclass=Singleton):
+    __os_client = OpenSubtitles()
+
+    @staticmethod
+    def get_client():
+        if not ScriBa.__os_client.is_connected():
+            print('OpenSubtitles client disconnected. Logging in...')
+            print('Token: {0}'.format(ScriBa.__os_client.login(user_agent='ScriBa v1.0')))
+        return ScriBa.__os_client
+
+
+def iterate_after_dropping(iterable, elements_to_drop):
+    return (l for i, l in dropwhile(lambda x: x[0] < elements_to_drop, enumerate(iterable)))
+
+
+def get_sub_from_encoded_data(encoded_data):
+    return decompress_gzip(base64.b64decode(encoded_data))
 
 
 def get_md5(file_path):
     return hashlib.md5(open(file_path, 'rb').read()).hexdigest()  # memory inefficient, but ok for small files, like srt
 
 
-def download_file_from_url(url, file_path):
-    u = urllib2.urlopen(url)
+def download_file_from_url(url):
+    with urllib.request.urlopen(url) as response:
+        return response.read()
 
-    with open(file_path, 'wb') as f:
-        meta = u.info()
-        meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
-        meta_length = meta_func("Content-Length")
-        file_size = None
-        if meta_length:
-            file_size = int(meta_length[0])
-        print("Downloading: {0} Bytes: {1}".format(url, file_size))
 
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            f.write(buffer)
-
-            status = "{0:16}".format(file_size_dl)
-            if file_size:
-                status += "   [{0:6.2f}%]".format(file_size_dl * 100 / file_size)
-            status += chr(13)
-            print(status, end="")
-        print()
-
-    return file_path
+def decompress_gzip(data):
+    return zlib.decompress(data, 16 + zlib.MAX_WBITS)
 
 # Given a IMDB ID return a list of dictionaries.
 # Each dictionary contains the IDSubtitleFile and the SubDownloadLink
 
 
-# TODO assert number of links for subs = number of declared CD
+# TODO assert number of links for subs = number of declared CDs
 def find_best_movie_subtitles(movie_subs):
     # Sort data by SubSumCD value
     sorted_data = sorted(movie_subs, key=lambda k: k['SubSumCD'])
